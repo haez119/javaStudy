ea3d534d51b5423197a394264d4fd06602df7b89
haez <hae_z@kakao.com>
2020-08-10 11:40:46 +0900

    first commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, welcome to the java world!");
+		System.out.println("Nice meet you!");
+		int myAge = 28;
+		System.out.println("My age is " + myAge);
+	}
+}
